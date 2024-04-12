d7f2129c2b6c468c8ae5d66e563e3d660f201be6
Vostanis <kimonvostanis@gmail.com>
2024-04-12 16:30:14 +0100

    macro updated
    simple.rs succesful
    yfinance.rs failing because of ;
    Type is handled by , but  is not

--- macros/src/lib.rs
+++ macros/src/lib.rs
@@ -1,7 +1,7 @@
 use proc_macro::TokenStream;
 use quote::quote;
 use syn::parse::{Parse, ParseStream, Result};
-use syn::{braced, bracketed, parse_macro_input, Attribute, Block, Stmt, Token, Type};
+use syn::{braced, bracketed, parse_macro_input, Attribute, Block, Ident, Stmt, Token, Type, TypePath};
 
 #[proc_macro]
 pub fn etl(input: TokenStream) -> TokenStream {
@@ -23,23 +23,27 @@ pub fn etl(input: TokenStream) -> TokenStream {
 }
 
 struct Arg {
-    type_one: Type,
-    type_two: Type,
+    type_one: Ident,
+    type_two: Ident,
     stmts: Vec<Stmt>,
 }
 
 impl Parse for Arg {
     fn parse(input: ParseStream) -> Result<Self> {
 
-        // #[Input -> Output]
-        input.parse::<Token![#]>()?;
-        let bracket_content;
-        let _bracket_token = bracketed!(bracket_content in input);
-        // let _inner_bracket_tokens = bracket_content.call(Attribute::parse_inner)?;
-        let type_one: Type = bracket_content.parse()?;
-        bracket_content.parse::<Token![-]>()?;
-        bracket_content.parse::<Token![>]>()?;
-        let type_two: Type = bracket_content.parse()?;
+        // @ Input -> Output
+        input.parse::<Token![@]>()?;
+        let type_one: Ident = input.parse()?;
+        input.parse::<Token![->]>()?;
+        let type_two: Ident = input.parse()?;
+
+        // @[Input -> Output]
+        // let bracket_content;
+        // let _bracket_token = bracketed!(bracket_content in input);
+        // let type_one: Type = bracket_content.parse()?;
+        // bracket_content.parse::<Token![-]>()?;
+        // bracket_content.parse::<Token![>]>()?;
+        // let type_two: Type = bracket_content.parse()?;
 
         // { async fn func() { ... } ... }
         let brace_content;
